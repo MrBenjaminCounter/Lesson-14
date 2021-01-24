ac6c0da05adea6b6e51f21c0cbf44064f5ba6595
MrBenjaminCounter <77814190+MrBenjaminCounter@users.noreply.github.com>
2021-01-24 08:27:52 +0000

    Update Challenge1.py
    Challenge One with extra formatting

--- Challenge1.py
+++ Challenge1.py
@@ -1,10 +1,9 @@
 #-----------------------------------------------------------------------------------------------------#
 
 import statistics
-import matplotlib
 from time import sleep as s
 from os import system, name
-import matplotlib.pyplot as plt
+
 
 
 #-----------------------------------------------------------------------------------------------------#
@@ -16,6 +15,18 @@ marks = [90, 88, 50, 60, 100, 75, 95]
 #-----------------------------------------------------------------------------------------------------#
 
 
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'     # Just to make the output more colorful 
+    RED = '\033[91m'
+    WHITE = '\033[0m'  
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
+    
 
 """
 
@@ -27,45 +38,75 @@ marks = [90, 88, 50, 60, 100, 75, 95]
 
 
 # Part 1
+print("")
+print("")
+print(bcolors.BOLD + bcolors.UNDERLINE + "Task One" + bcolors.WHITE)
 total = 0
 for i in range(len(ages)):
   total += ages[i]
-print(total)
+print("")
 average = total/len(ages)
-print(round(average,2))
+print("")
+print("The average age of the students is: ")
+print("")
+print(bcolors.GREEN + str(round(average,2))+ bcolors.WHITE)
+print("")
+print("")
 
 # Part 2
+print(bcolors.BOLD + bcolors.UNDERLINE + "Task Two" + bcolors.WHITE)
+print("")
+print(bcolors.WHITE + "The average mark of the students is: ")
+print("")
 average = sum(marks)/len(marks)
-print(round(average,2))
+print(bcolors.GREEN + str(round(average,2)) + bcolors.WHITE)
 
 # Part 3 + 4 
+print("")
+print("")
+print(bcolors.UNDERLINE + bcolors.BOLD + "Task Three and Four" + bcolors.WHITE)
+print("")
+print(bcolors.WHITE + "The highest and lowest ages in the list are: " + bcolors.GREEN)
+print("")
 print(min(ages))
 print(max(ages))
-print(ages.index(min(ages)))
-print(ages.index(max(ages)))
+print("")
+print(bcolors.WHITE + "The students' names of these ages are: " + bcolors.GREEN)
+print("")
 print(students[ages.index(min(ages))])
-print(students[ages.index(max(ages))])
+print(students[ages.index(max(ages))] + bcolors.WHITE)
 
-# Part 3 + 4 
-youngest = 100
-youngestIndex = -1
-oldest = 0
-oldestIndex = -1
-for i in range(len(ages)):
-  if ages[i] < youngest:
-    youngest = ages[i]
-    youngestIndex = i
-  if ages[i] > oldest:
-    oldest = ages[i]
-    oldestIndex = i
+# # Part 3 + 4 
+# youngest = 100
+# youngestIndex = -1
+# oldest = 0
+# oldestIndex = -1
+# for i in range(len(ages)):
+#   if ages[i] < youngest:
+#     youngest = ages[i]
+#     youngestIndex = i
+#   if ages[i] > oldest:
+#     oldest = ages[i]
+#     oldestIndex = i
 
-print(students[youngestIndex],":",youngest)
-print(students[oldestIndex],":",oldest)
+# print(students[youngestIndex],":",youngest)
+# print(students[oldestIndex],":",oldest)
 
 # Part 5
+print("")
+print("")
+print(bcolors.UNDERLINE + bcolors.BOLD + "Task Five" + bcolors.WHITE)
 orderedMarks = sorted(marks)
-print(orderedMarks)
-print(orderedMarks[len(orderedMarks)//2])
+print("")
+print(bcolors.GREEN + "The median mark is: ")
+print("")
+print(str(orderedMarks[len(orderedMarks)//2]) + bcolors.WHITE)
+
+
+
+
+
+
 
 
 
